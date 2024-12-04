fd84c4551193ca8cf66815e6f87b8fb60e5c3f09
Alfarizky Sunetyo <alfarizky1009@gmail.com>
2024-12-04 22:31:02 +0700

    Change column type

--- models/books.go
+++ models/books.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 // Golang can't understand json response 3rd string is to point the json field
 type Books struct {
-	Id        uint    `json:"id" gorm:"unique;primaryKey;autoIncrement"`
+	Id        uint    `json:"id" gorm:"unique;primaryKey;autoIncrement;notNull"`
 	Author    *string `json:"author"`
 	Title     *string `json:"title"`
 	Publisher *string `json:"publisher"`
